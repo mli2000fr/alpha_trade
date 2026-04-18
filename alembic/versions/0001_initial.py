cfd8cd7b2741948ba95d82e7430bf96a82caa6fe
PC MLI <mli.2000.f@gmail.com>
2026-04-18 22:00:11 +0200

    dette tech Pas de migration DB (Alembic absent)

---
+++ alembic/versions/0001_initial.py
@@ -0,0 +1,22 @@
+"""Initial migration
+
+Revision ID: 0001_initial
+Revises: 
+Create Date: 2026-04-18
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+# revision identifiers, used by Alembic.
+revision = '0001_initial'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+def upgrade():
+    pass  # Ajoutez ici la création des tables si besoin
+
+def downgrade():
+    pass
+
